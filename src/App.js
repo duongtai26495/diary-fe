87a5d55ad336288a307b60676f19150054391719
taiduong.laptop <taiduong.264@gmail.com>
2022-11-24 19:34:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header-wrapper">
         <Header />
       </header>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full'>
-        
+    <div className='container m-auto'>
+        <div className='header-logo'>
+          <img className='image-logo' src='images/logo.png' />
+        </div>
+        <ul className='menu-wrapper'>
+          <li className='menu-item'>Home</li>
+          <li className='menu-item'>Category</li>
+          <li className='menu-item'>Contact</li>
+          <li className='menu-item'>About us</li>
+        </ul>
     </div>
   )
 }
